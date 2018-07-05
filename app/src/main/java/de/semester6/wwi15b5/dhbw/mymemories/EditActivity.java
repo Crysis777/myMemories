3ef4be40f80fa1327d28c3ba18b608d8c2d16ef7
Crysis777 <pfeiffer.matthias@student.dhbw-karlsruhe.de>
2018-07-05 16:29:19 +0200

    Starting project

--- app/src/main/java/de/semester6/wwi15b5/dhbw/mymemories/EditActivity.java
+++ app/src/main/java/de/semester6/wwi15b5/dhbw/mymemories/EditActivity.java
@@ -1,8 +1,13 @@
 package de.semester6.wwi15b5.dhbw.mymemories;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -17,5 +22,15 @@ public class EditActivity extends AppCompatActivity {
         myEdit.setText(data);
 
         oldFileName = data;
+    }
+
+    public void onDeleteClickE(View view) {
+        oldFileName += ".mp4";
+        File file = new File(getDir("memos", MODE_PRIVATE), oldFileName);
+        file.delete();
+    }
+
+    public void onSaveClickE(View view) {
+
     }
 }
