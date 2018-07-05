0e85ac640fa94faa104c8c542d916216e256ae0c
Crysis777 <pfeiffer.matthias@student.dhbw-karlsruhe.de>
2018-07-05 15:41:29 +0200

    Starting project

--- app/src/main/java/de/semester6/wwi15b5/dhbw/mymemories/MainMemoBrowse.java
+++ app/src/main/java/de/semester6/wwi15b5/dhbw/mymemories/MainMemoBrowse.java
@@ -1,11 +1,14 @@
 package de.semester6.wwi15b5.dhbw.mymemories;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -13,6 +16,7 @@ import java.util.ArrayList;
 public class MainMemoBrowse extends AppCompatActivity {
 
     private RecyclerView recView;
+    private SearchView searchView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     static ArrayList<String> textArray = new ArrayList<>();
@@ -22,7 +26,9 @@ public class MainMemoBrowse extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_memo_browse);
 
-        recView = (RecyclerView) findViewById(R.id.recView1);
+        recView = findViewById(R.id.recView1);
+        //searchView = findViewById(R.id.searchView1);
+
         mLayoutManager = new LinearLayoutManager(this);
         recView.setLayoutManager(mLayoutManager);
     }
@@ -39,6 +45,11 @@ public class MainMemoBrowse extends AppCompatActivity {
         recView.setAdapter(mAdapter);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
     private void loadFileList(){
         File dir;
         dir = getDir("memos", MODE_PRIVATE);
@@ -46,17 +57,10 @@ public class MainMemoBrowse extends AppCompatActivity {
         File[] subFiles = dir.listFiles();
 
         if (subFiles != null) {
-            int i = 0;
             for (File file : subFiles) {
-                textArray.add(file.getName());
-                i++;
+                textArray.add(file.getName().substring(0, file.getName().length() - 4));
             }
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
     }
 
     public void onRecordClick(View view) {
@@ -64,13 +68,16 @@ public class MainMemoBrowse extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //TODO Implement filter function.
     public void onFilterClick(View view) {
-        //TODO Implement filter function.
         //This is just test code
 
     }
 
-
     //TODO Implement search functionality
-
+    public void onSearchClick(View view) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, "searched", duration).show();
+    }
 }
