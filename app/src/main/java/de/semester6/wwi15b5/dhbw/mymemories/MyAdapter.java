ee17ba4f6b8e8a71b0f0ba727b06095773d710cd
Crysis777 <pfeiffer.matthias@student.dhbw-karlsruhe.de>
2018-07-05 18:14:35 +0200

    Starting project

--- app/src/main/java/de/semester6/wwi15b5/dhbw/mymemories/MyAdapter.java
+++ app/src/main/java/de/semester6/wwi15b5/dhbw/mymemories/MyAdapter.java
@@ -2,20 +2,32 @@ package de.semester6.wwi15b5.dhbw.mymemories;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.Image;
+import android.media.MediaPlayer;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.util.Log;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
+    private static final String LOG_TAG = "AudioPlayTest";
     private ArrayList<String> texte;
+    private boolean bStartPlaying = true;
+    private MediaPlayer bPlayer = null;
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
@@ -31,7 +43,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
             itemText = (TextView) itemView.findViewById(R.id.info_text);
             editView = (ImageView) itemView.findViewById(R.id.imageViewEdit);
-            playView = (ImageView) itemView.findViewById(R.id.imageViewEdit);
+            //playView = (ImageView) itemView.findViewById(R.id.imageViewEdit);
 
         }
     }
@@ -65,7 +77,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         };
         holder.editView.setOnClickListener(onClickListenerEdit);
 
-        final View.OnClickListener onClickListenerPlay = new View.OnClickListener() {
+/*        final View.OnClickListener onClickListenerPlay = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(view.equals(holder.playView)){
@@ -74,14 +86,45 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             }
         };
         holder.playView.setOnClickListener(onClickListenerPlay);
-
-
+*/
     }
-
+/*
     public void onClickPlay(String memoText, View view){
-
+        onPlayB(bStartPlaying, memoText);
+        if (bStartPlaying) {
+            //Change button to pause icon
+        } else {
+            //Change button to play icon
+        }
+        bStartPlaying = !bStartPlaying;
     }
 
+    private void onPlayB(boolean start, String memoText) {
+        if (start) {
+            startPlayingB(memoText);
+        } else {
+            stopPlayingB();
+        }
+    }
+
+    private void startPlayingB(String memoText) {
+        bPlayer = new MediaPlayer();
+        String bFileName = null;
+        bFileName = memoText + ".mp4";
+        try {
+            bPlayer.setDataSource(bFileName);
+            bPlayer.prepare();
+            bPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlayingB() {
+        bPlayer.release();
+        bPlayer = null;
+    }
+*/
     public void onClickEdit(int pos, String memoText, View view){
         Intent intent = new Intent(view.getContext(), EditActivity.class);
         intent.putExtra("memoText", memoText);
